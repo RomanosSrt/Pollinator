f4e5db7312b43e61a9424754c3fd3008bec4a25a
RomanosSrt <rpsarantidis@gmail.com>
2026-05-18 14:59:25 +0300

    Front init
    React + Typescript + Tailwind@4 + Vite

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+        <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello React + TypeScript + Tailwind
+      </h1>
+    </div>
+  )
+}
+
+export default App
